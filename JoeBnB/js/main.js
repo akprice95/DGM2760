4663d3a9c9549a4a6230adcb87e40ad96ab5395a
Unknown <akprice95@msn.com>
2017-11-09 19:09:01 -0700

    Module 1

---
+++ JoeBnB/js/main.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+/*jslint browser:true */
+"use strict()";
+
+
+var userName = prompt("What is your name?") ;
+
+var showDate = new Date();
+var myDateString = new Date();
+
+document.getElementById("message").innerHTML = myDateString;
+document.getElementById("companyName").innerHTML = "Joe's Local Bed and Breakfast";
+document.getElementById("quote").innerHTML = "“And now leave me in peace for a bit! I don't want to answer a string of questions while I am eating. I want to think!";
+document.getElementById("userName").innerHTML = "Welcome " + userName + " to Joe's BnB";
+document.getElementById("userName").innerHTML = "Welcome " + userName + " to Joe's BnB";
