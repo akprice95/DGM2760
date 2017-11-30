fa3ba1e0d849bf34e18b0492a37dfcd66bd430bd
Unknown <akprice95@msn.com>
2017-11-29 21:04:52 -0700

    Module 10

--- Menu Duplication/js/main.js
+++ Menu Duplication/js/main.js
@@ -1,2 +1,24 @@
 /*jslint browser:true */
 "use strict";
+
+var temp = document.querySelectorAll('ul#primaryNavigation li a');
+
+
+
+function onstart(){
+for(var i = 0; i < temp.length; i++){
+var temp2 = temp[i].getAttribute("href");
+var temp3 = temp[i].text;
+var diaper = document.createElement("A");
+diaper.setAttribute('href', temp2);
+diaper.innerHTML = temp3;
+var baby = document.createElement("LI");
+
+
+baby.appendChild(diaper);
+document.getElementById('smallArea').appendChild(baby);
+
+}
+}
+onstart();
+
